55d7ed813c81a331ad7e7f38fdef3c867b702657
Youngmi Lee <abinding@gmail.com>
2025-01-03 08:44:26 -0600

    changing from 6 to 5 for laptop since limit=10 in backenend

--- src/pages/User/EventList.js
+++ src/pages/User/EventList.js
@@ -3,7 +3,7 @@ import React from "react";
 const EventList = ({ events }) => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+      <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
         {events.map((event) => (
           <div
             key={event.id}
